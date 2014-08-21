208e0c0baf2ef517cc7b589dddb5842efad5259a
Max Calabrese <max.calabrese@ymail.com>
2014-08-21 14:07:53 +0200

    install browserify

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,16 @@
 var gulp = require('gulp');
 var plugins = require('gulp-load-plugins')();
+var package = require('./package.json');
 
 gulp.task('default', function() {
   // place code for your default task here
+});
+
+gulp.task('browserify', function(){
+    gulp.src('src/js/windtalkers.js')
+        .pipe(plugins.browserify({
+            insertGlobals : true,
+            debug : process.env.NODE_ENV !== 'production'
+        }))
+        .pipe(gulp.dest('./build/js'))
 });
