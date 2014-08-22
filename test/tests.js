005e251d3e8aef44bbeaa3dfce0e9ebd4f308303
Max Calabrese <max.calabrese@ymail.com>
2014-08-22 22:27:22 +0200

    Force load sinon FakeXMLHttpRequest

--- test/tests.js
+++ test/tests.js
@@ -2,5 +2,17 @@ global.chai = require('chai');
 global.expect = require('chai').expect;
 global.sinon = require('sinon');
 
+// Sinon refuses to load FakeXMLHttpRequest and FakeServer on node.js so we load them manually:
+if (!sinon.useFakeXMLHttpRequest) require('sinon/lib/sinon/util/fake_xml_http_request');
+if (!sinon.fakeServer) require('sinon/lib/sinon/util/fake_server');
+
+var $ = global.jQuery || require('jquery');
+
 // Require test files here:
 require('./framework/container.test.js');
+require('./framework/api_client.test.js');
+
+$(function() {
+    if (window.mochaPhantomJS) { mochaPhantomJS.run(); }
+    else { mocha.run(); }
+});
