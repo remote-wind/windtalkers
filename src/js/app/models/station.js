7cfcc0c6641bebfb9fecedcafb3d9520247a9f3f
Max Calabrese <max.calabrese@ymail.com>
2014-08-26 01:01:31 +0200

    migrate stations code from jasmine codebase

--- src/js/app/models/station.js
+++ src/js/app/models/station.js
@@ -1,18 +1,31 @@
 "use strict";
 
 var Model = require('windtalkers/framework/model');
+var Observation = require('windtalkers/app/models/observation');
 /**
  * @constructor does not require use of `new` keyword.
  */
-function Station(attrs){
-
-    attrs = attrs || {};
-
+function Station(attributes){
+    if (attributes) {
+        attributes =_.extend(attributes, {
+            latestObservation: attributes["latest_observation"] ? Observation(attributes["latest_observation"]["observation"]) : null
+        });
+    }
     // "super" constructor call
-    return Station.prototype.create(attrs);
+    return Station.prototype.create(attributes);
 }
 
-
-
-Model.prototype.extend(Station);
+Model.prototype.extend(Station, {
+    /**
+     * Overrides Object.toString method to output the name of the station
+     * @returns {string}
+     */
+    toString : function() {
+        if (this.offline) {
+            return this.name + ' <br> ' + 'Offline'
+        } else if (this.latestObservation) {
+            return this.name + ' <br> ' + this.latestObservation.windSpeed();
+        }
+    }
+});
 module.exports = Station;
--- test/app/models/station.test.js
+++ test/app/models/station.test.js
@@ -7,7 +7,44 @@ describe("App/Models/Station", function(){
     });
 
     beforeEach(function(){
-        this.obj = Station({ name: 'Test Station' })
+        this.station = Station({
+            name: 'Test Station',
+            url: 'Foo',
+            offline: false,
+            latest_observation : {
+                observation : {
+                    cardinal : "E",
+                    id : 45083,
+                    max_wind_speed : 3,
+                    min_wind_speed : 1,
+                    speed : 2,
+                    direction: 90
+                }
+            }
+        });
+    });
+
+    it("responds to name", function(){
+        expect(this.station.name).to.equal('Test Station');
+    });
+    it("responds to url", function(){
+        expect(this.station.url).to.equal('Foo');
+    });
+
+    describe(".latestObservation", function(){
+        it("should convert the latest observation into an observation", function(){
+            expect(this.station.latestObservation.id).to.equal(45083);
+        });
+    });
+
+    describe(".toString", function(){
+        it("includes the wind speed of an online station", function(){
+            expect(this.station.toString()).to.equal('Test Station <br> 2&thinsp;(1-3) ms');
+        });
+        it("tells you that a station is offline", function(){
+            this.station.offline = true;
+            expect(this.station.toString()).to.equal('Test Station <br> Offline');
+        });
     });
 
 });
