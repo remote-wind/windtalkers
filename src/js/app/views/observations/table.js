81d630c2949524381db49d1995f8fead547d7c5f
Max Calabrese <max.calabrese@ymail.com>
2014-09-11 20:44:46 +0200

    migrate table view and add specs

--- src/js/app/views/observations/table.js
+++ src/js/app/views/observations/table.js
@@ -1,15 +1,89 @@
 "use strict";
 
 var View = require('windtalkers/framework/view');
-
 /**
- *
+ * @param {Object} options
  * @constructor
  */
-function TableView(){
+function TableView(options){
+    options = _.defaults(options || {}, {
+        per_page: 20
+    });
+    /**
+     * Bind event handlers for pagination
+     * @param {jQuery} template
+     * @returns {jQuery}
+     */
+    function paginate(template, options) {
+        var observations = template.find('.observation');
+        var pagination = template.find('.pagination');
+        var per_page = options.per_page;
+
+        // add page classes
+        observations.each(function(i){
+            $(this).addClass('page-' + Math.floor(i/per_page + 1));
+        });
+        // Mark first page as active
+        template.find('.pagination li:first').addClass('active');
+        template.find('.observation:not(.page-1)').addClass('hidden');
+
+        // when clicking a page number
+        pagination.on('click', '.page', function(){
+            var on_page = $(this).attr('href').replace('#', '.');
+            pagination.find('li').removeClass('active');
+            $(this).parent().addClass('active');
+            observations.filter(on_page).removeClass('hidden');
+            observations.not(on_page).addClass('hidden');
+            return false;
+        });
+        return template;
+    }
+
     return TableView.prototype.create({
-        template : _.template('<table></table>')
+        options: options,
+        render: function(view_data){
+            var per_page = this.options.per_page;
+            view_data = _.defaults(view_data, {
+                per_page: per_page,
+                pages: Math.ceil(view_data.observations.length / per_page)
+            });
+            return paginate( TableView.prototype.render(view_data), options )
+        }
     })
 }
 
-module.exports = View.prototype.extend(View, TableView, {});
+module.exports = View.prototype.extend(View, TableView, {
+    defaultTranslations: {
+        created_at: 'Time',
+        speed: 'Wind speed',
+        direction: 'Direction'
+    },
+    template: _.template(
+        '<table>' +
+            '<legend class="station-name"><%= this.station.name %></legend>' +
+            '<thead>' +
+                '<tr>' +
+                    '<td><%= t.created_at %></td>' +
+                    '<td><%= t.speed %></td>' +
+                    '<td><%= t.direction %></td>' +
+                '</tr>' +
+            '</thead>' +
+            '<tbody>' +
+                '<% _.each(this.observations, function(obs, index) { %>' +
+                '<tr class="observation" >' +
+                    "<td class='created-at'><%= obs.dateTime() %></td>" +
+                    "<td class='wind-speed'><%= obs.windSpeed() %></td>" +
+                    "<td class='direction'><%= obs.degreesAndCardinal() %></td>" +
+                '</tr>'+
+                '<% }); %>' +
+            '</tbody>' +
+        '</table>' +
+        '<nav class="pages">' +
+            '<ul class="pagination">' +
+            '<% _.times(this.pages, function(page){ page++; %>' +
+                '<li><a class="page" href="#page-<%= page %>"><%= page %></a></li>' +
+            '<% }); %>' +
+            '</ul>' +
+        '</nav>'
+    )
+});
--- test/app/views/observations/table.test.js
+++ test/app/views/observations/table.test.js
@@ -3,12 +3,67 @@
 describe("App/Views/Observations/Table", function(){
 
     var TableView = require('windtalkers/app/views/observations/table');
+    var Observation = require('windtalkers/app/models/observation');
+
 
     before(function(){
-       this.view = TableView();
+        this.sandbox = $('#sandbox');
+        $('body').append(this.sandbox);
     });
 
-    it("creates a new instance without new", function(){
-        expect(this.view).to.be.an.instanceof(TableView);
+    beforeEach(function(){
+
+        this.sandbox.empty();
+
+        this.observation = Observation({
+           "cardinal" : "E",
+           "created_at" : "2014-04-17T08:11:18Z",
+           "direction" : 90.0,
+           "id" : 45083,
+           "max_wind_speed" : 5,
+           "min_wind_speed" : 0.5,
+           "speed" : 2,
+           "station_id" : 4,
+           "tstamp" : 1397722278
+       });
+       this.view = TableView({ per_page: 1 });
+       this.rendered = this.view.render({
+           station: { name: 'Test Station' },
+           observations : [this.observation, this.observation]
+       });
+       this.sandbox.append(this.rendered);
+    });
+
+    after(function(){
+        this.sandbox.remove();
+    });
+
+
+    describe("render", function(){
+        it("displays the stations name", function(){
+            expect(this.rendered.text()).to.contain('Test Station');
+        });
+        it("displays the wind speed", function(){
+            expect(this.rendered.text()).to.contain('2 (0.5-5) ms');
+        });
+        it("displays the direction", function(){
+            expect(this.rendered.text()).to.contain('E (90°)');
+        });
+    });
+
+    describe("pagination", function(){
+        it("adds pagination links", function(){
+           expect(this.rendered.find('a.page').length).to.eq(2)
+        });
+
+        it("shows observations on page", function(){
+            this.rendered.find('a.page:last').click();
+            expect(this.rendered.find('.observation:last').hasClass('hidden')).to.beFalse
+        });
+
+        it("hides observations on not on page", function(){
+            this.rendered.find('a.page:last').click();
+            expect(this.rendered.find('.observation:first').hasClass('hidden')).to.beTrue
+        });
     });
 });
